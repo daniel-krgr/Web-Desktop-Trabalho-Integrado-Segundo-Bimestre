f4e019090c764044a7589aed826c3b78ffcd569d
daniel-krgr <daniel-krugerdeal@hotmail.com>
2025-05-28 19:25:34 -0300

    Initial commit

---
+++ Desktop_Trabalho_Integrado/src/main/java/br/unipar/desktop_trabalho_integrado/Desktop_Trabalho_Integrado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.unipar.desktop_trabalho_integrado;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Desktop_Trabalho_Integrado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Web_Trabalho_Integrado/src/main/java/br/unipar/web_trabalho_integrado/HelloApplication.java
@@ -0,0 +1,9 @@
+package br.unipar.web_trabalho_integrado;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
