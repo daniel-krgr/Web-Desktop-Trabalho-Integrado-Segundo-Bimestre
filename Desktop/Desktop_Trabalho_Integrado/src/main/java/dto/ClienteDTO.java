45093eb96050d78fea7373864e8beb2451c476b4
daniel-krgr <daniel-krugerdeal@hotmail.com>
2025-06-08 17:20:43 -0300

    Update

---
+++ Desktop/Desktop_Trabalho_Integrado/src/main/java/dto/ClienteDTO.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dto;
+
+/**
+ *
+ * @author Daniel
+ */
+public class ClienteDTO {
+    
+    private String nome;
+    private String telefone;
+    private String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}
