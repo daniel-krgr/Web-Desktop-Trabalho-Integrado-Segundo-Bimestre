5888a5ed0827f440af226a6610c4e9c768fd2c51
daniel-krgr <daniel-krugerdeal@hotmail.com>
2025-06-08 16:59:06 -0300

    update

---
+++ Desktop/Desktop_Trabalho_Integrado/src/main/java/br/unipar/desktop_trabalho_integrado/Desktop_Trabalho_Integrado.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.unipar.desktop_trabalho_integrado;
+
+import javax.swing.JFrame;
+import view.ViewPrincipal;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Desktop_Trabalho_Integrado {
+
+    public static void main(String[] args) {
+        
+        ViewPrincipal view = new ViewPrincipal();
+        view.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        view.setVisible(true);
+        
+    }
+}
---
+++ Desktop/Desktop_Trabalho_Integrado/src/main/java/model/ItemVenda.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Daniel
+ */
+public class ItemVenda {
+    
+    private int id;
+    private String descricao;
+    private double valor;
+    private 
+    
+    
+    
+}
